8dd869fa9e2f001ddef95f068248ad7bfab09136
lucbea <profluciaferrer@gmail.com>
2024-06-17 20:00:43 -0300

    Ingreso de tarea sin guardado en LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-
-import './App.css'
+import { Container, CssBaseline } from "@mui/material"
+import { IngresoTarea } from './layouts/ingresoTarea/IngresoTarea'
+import { ListaTareas } from './layouts/listaTareas/ListaTareas'
 
 function App() {
- 
   return (
-    <> 
-    </>
+    <div>
+
+    {/* <ThemeProvider theme={ Theme }> */}
+      <CssBaseline/>
+      <Container sx={{ margin:'0px', padding:'0px', marginInline:'auto'}} >
+        <h2 style={{textAlign: 'center', marginBottom: '10px'}} >LISTA  DE  TAREAS</h2>
+        <IngresoTarea />
+        <ListaTareas/>
+      </Container>
+     {/* </ThemeProvider> */}
+     
+    </div>
   )
 }
 
